cf12550367f9a1a6997c4207998afb97eb4fdf7a
Jose Maximino Chavez <josemc.ortiz@gmail.com>
2016-01-13 17:49:44 -0600

    Creacion de Poryecto!!!

---
+++ RegistroDeSalaDeEmergencias/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistroDeSalaDeEmergencias
+{
+    public partial class ResgistroDeSalaDeEmergencias : Form
+    {
+        public ResgistroDeSalaDeEmergencias()
+        {
+            InitializeComponent();
+        }
+    }
+}
